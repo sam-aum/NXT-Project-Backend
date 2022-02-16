4233cd5210be34eca63648eb37bb119706601737
Sam-Aum <psaum01@gmail.com>
2022-02-16 10:31:28 -0500

    env

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017/NXTDB'
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/NXTDB'
 
 mongoose.connect(MONGO_URI, ()=>{
     console.log('connected to MongoDB - NXTDB')
