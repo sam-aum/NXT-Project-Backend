4dfe3f088aa14beda67653f131bdc38870664b28
Sam-Aum <psaum01@gmail.com>
2022-02-16 10:59:02 -0500

    heroku code update

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/NXTDB'
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://admin:<password>@cluster0.yh3qq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 mongoose.connect(MONGO_URI, ()=>{
     console.log('connected to MongoDB - NXTDB')
